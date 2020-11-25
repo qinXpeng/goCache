e2a5bd13c6f397e76d67de496f82bbe4d1a80fc9
qinpeng03 <qinpeng03@baidu.com>
2020-11-25 15:48:32 +0800

    fix

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module goCache
 
 go 1.15
+
+require github.com/golang/protobuf v1.4.3
--- gocache/maincache.go
+++ gocache/maincache.go
@@ -2,9 +2,19 @@ package gocache
 
 import (
 	"fmt"
+	pb "goCache/gocachepb"
+	"goCache/singleflight"
 	"log"
 	"sync"
 )
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+	peers     PeerPicker
+	loader    *singleflight.Group
+}
 
 type Getter interface {
 	Get(key string) ([]byte, error)
@@ -12,14 +22,8 @@ type Getter interface {
 
 type GetterFunc func(key string) ([]byte, error)
 
-func (fc GetterFunc) Get(key string) ([]byte, error) {
-	return fc(key)
-}
-
-type Group struct {
-	name      string
-	getter    Getter
-	mainCache cache
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
 }
 
 var (
@@ -37,10 +41,12 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
 }
+
 func GetGroup(name string) *Group {
 	mu.RLock()
 	g := groups[name]
@@ -50,20 +56,68 @@ func GetGroup(name string) *Group {
 
 func (g *Group) Get(key string) (ByteView, error) {
 	if key == "" {
-		return ByteView{}, fmt.Errorf("key is nil")
+		return ByteView{}, fmt.Errorf("key is required")
 	}
-	if ele, ok := g.mainCache.get(key); ok {
-		log.Println("[goCache] hit")
-		return ele, nil
+
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
 	}
-	return g.getLocal(key)
+
+	return g.load(key)
 }
-func (g *Group) getLocal(key string) (ByteView, error) {
+
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[GeeCache] Failed to get from peer", err)
+			}
+		}
+
+		return g.getLocally(key)
+	})
+
+	if err == nil {
+		return viewi.(ByteView), nil
+	}
+	return
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.put(key, value)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
 	bytes, err := g.getter.Get(key)
 	if err != nil {
 		return ByteView{}, err
+
 	}
-	val := ByteView{b: bytes}
-	g.mainCache.put(key, val)
-	return val, nil
+	value := ByteView{b: copyByte(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	req := &pb.Request{
+		Group: g.name,
+		Key:   key,
+	}
+	res := &pb.Response{}
+	err := peer.Get(req, res)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: res.Value}, nil
 }
